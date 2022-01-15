c21cf1841fe2035093df179c16082cb9865f69f0
Luis Loro <luis.loro@outlook.com.br>
2022-01-15 12:33:34 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,14 +3,26 @@ import MovieCard from 'components/MovieCard';
 import './styles.css';
 import axios from 'axios';
 import {BASE_URL} from 'utils/requests';
+import {useState, useEffect} from 'react';
+import {MoviePage} from 'types/movies';
+
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies`).then(response => {
-        console.log(response.data);
-    });
+    const[pageNumber,setPageNumber] = useState(0);
+
+    useEffect(() =>{
+        axios.get(`${BASE_URL}/movies`).then(response => {
+            const data = response.data as MoviePage;
+            setPageNumber(data.number)
+        });
+    },[]);
+
+
+    
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
